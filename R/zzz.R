3775db8261fc7f966959e0ebd33a563ebc68b703
lshep <lori.shepherd@roswellpark.org>
2024-12-17 11:20:20 -0500

    Deprectaed - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,9 @@
   ## disable ShortRead OpenMP (which crashes FastqStreamer when invoked inside a mcparallel() statement)
   .Call(ShortRead:::.set_omp_threads, 1L)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.22")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
